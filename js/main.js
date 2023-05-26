0b346ee87bda84a8afa2bc1ee847a68794362c4e
Brenton <brentontreb@gmail.com>
2023-05-26 11:19:19 -0600

    Fixed initial ball movement and made page in dark mode with bootstrap

--- js/main.js
+++ js/main.js
@@ -6,11 +6,12 @@ import { BrickField } from "./brickField.js";
 const canvas = document.getElementById("myCanvas");
 const ctx = canvas.getContext("2d");
 
-const ball = new Ball((canvas.width / 2), (canvas.height - 30), 10, 0, (Math.PI * 2), "blue", 2, 2);
+const ball = new Ball((canvas.width / 2), (canvas.height - 30), 10, 0, (Math.PI * 2), "blue", 2, -2);
 const paddle = new Paddle(canvas, 75, 10, "red", 7)
 const brick = new Brick(0, 0, 1, 75, 20, "green");
 const brickField = new BrickField(10, 3, 5, 30, 30);
 
+// Fill the brickfield with bricks
 brickField.init(brick);
 
 const interval = setInterval(draw, 10);
@@ -32,6 +33,6 @@ function draw() {
     brickField.collisionDetection(ball);
 }
 
-// need to add bind or it will not work
+// Need to add bind or it will not work
 document.addEventListener("keydown", paddle.keyDownHandler.bind(paddle), false);
 document.addEventListener("keyup", paddle.keyUpHandler.bind(paddle), false);
